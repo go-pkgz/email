17d4a5022ef4c20dfacccf0a9f8c9c2143968cf7
Vitaliy Utkin <mail@102web.ru>
2022-04-08 12:24:15 +0500

    add email attachments

--- email_test.go
+++ email_test.go
@@ -2,10 +2,13 @@ package email
 
 import (
 	"bytes"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/smtp"
+	"os"
 	"testing"
 	"time"
 
@@ -62,7 +65,7 @@ func TestEmail_Send(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	expBody := "From: from@example.com\nTo: to@example.com\nSubject: subj\nContent-Transfer-Encoding: quoted-printable\nMIME-version: 1.0\nContent-Type: text/html; charset=\"UTF-8\"\nDate: Thu, 10 Feb 2022 23:33:58 +0000\n\nsome text\r\n"
+	expBody := "From: from@example.com\nTo: to@example.com\nSubject: subj\nMIME-version: 1.0\nDate: Thu, 10 Feb 2022 23:33:58 +0000\nContent-Transfer-Encoding: quoted-printable\nContent-Type: text/html; charset=\"UTF-8\"\n\nsome text\r\n"
 	assert.Equal(t, expBody, wc.buff.String())
 
 	require.Equal(t, 1, len(smtpClient.MailCalls()))
@@ -234,10 +237,117 @@ func TestEmail_buildMessageWithMIME(t *testing.T) {
 		Subject: "subj",
 	})
 	require.NoError(t, err)
-	assert.Contains(t, msg, "From: from@example.com\nTo: to@example.com\nSubject: subj\nContent-Transfer-Encoding: quoted-printable\nMIME-version: 1.0\nContent-Type: text/html; charset=\"UTF-8\"", msg)
-	assert.Contains(t, msg, "\n\nthis is a test\r\n12345", msg)
+	assert.Contains(t, msg, "Content-Transfer-Encoding: quoted-printable\nContent-Type: text/html; charset=\"UTF-8\"", msg)
+	assert.Contains(t, msg, "From: from@example.com\nTo: to@example.com\nSubject: subj\nMIME-version: 1.0", msg)
+	assert.Contains(t, msg, "\n\nthis is a test\r\n12345\r\n", msg)
 	assert.Contains(t, msg, "Date: ", msg)
 }
+
+func TestEmail_buildMessageWithMIMEAndAttachments(t *testing.T) {
+	l := &mocks.LoggerMock{LogfFunc: func(format string, args ...interface{}) {
+		fmt.Printf(format, args...)
+		fmt.Printf("\n")
+	}}
+
+	e := NewSender("localhost", ContentType("text/html"),
+		Port(2525),
+		Log(l))
+
+	msg, err := e.buildMessage("<div>this is a test mail with attachments\\n12345</div>\\n", Params{
+		From:        "from@example.com",
+		To:          []string{"to@example.com"},
+		Subject:     "test email with attachments",
+		Attachments: []string{"testdata/1.txt", "testdata/2.txt", "testdata/image.jpg"},
+	})
+	require.NoError(t, err)
+	assert.Contains(t, msg, "Content-Type", "multipart/mixed; boundary=", msg)
+	assert.Contains(t, msg, "Content-Disposition: attachment; filename=\"1.txt\"", msg)
+	assert.Contains(t, msg, "Content-Disposition: attachment; filename=\"2.txt\"", msg)
+	assert.Contains(t, msg, "Content-Disposition: attachment; filename=\"image.jpg\"", msg)
+
+	fData1, err := os.ReadFile("testdata/1.txt")
+	require.NoError(t, err)
+	fData2, err := os.ReadFile("testdata/2.txt")
+	require.NoError(t, err)
+	fData3, err := os.ReadFile("testdata/image.jpg")
+	require.NoError(t, err)
+
+	b1 := make([]byte, base64.StdEncoding.EncodedLen(len(fData1)))
+	base64.StdEncoding.Encode(b1, fData1)
+	b2 := make([]byte, base64.StdEncoding.EncodedLen(len(fData2)))
+	base64.StdEncoding.Encode(b2, fData2)
+	b3 := make([]byte, base64.StdEncoding.EncodedLen(len(fData3)))
+	base64.StdEncoding.Encode(b3, fData3)
+	assert.Contains(t, msg, string(b1), msg)
+	assert.Contains(t, msg, string(b2), msg)
+	assert.Contains(t, msg, string(b3), msg)
+}
+
+func TestEmail_buildMessageWithMIMEAndWrongAttachments(t *testing.T) {
+	l := &mocks.LoggerMock{LogfFunc: func(format string, args ...interface{}) {
+		fmt.Printf(format, args...)
+		fmt.Printf("\n")
+	}}
+
+	e := NewSender("localhost", ContentType("text/html"),
+		Port(2525),
+		Log(l))
+
+	msg, err := e.buildMessage("<div>this is a test mail with attachments\\n12345</div>\\n", Params{
+		From:        "from@example.com",
+		To:          []string{"to@example.com"},
+		Subject:     "test email with attachments",
+		Attachments: []string{"testdata/1.txt", "testdata/2.txt", "does/not/exist/1.txt"},
+	})
+	require.Error(t, err)
+	require.Equal(t, "failed to write attachments: "+
+		"open does/not/exist/1.txt: no such file or directory", err.Error())
+	require.Equal(t, "", msg)
+
+	msg, err = e.buildMessage("<div>this is a test mail with attachments\\n12345</div>\\n", Params{
+		From:        "from@example.com",
+		To:          []string{"to@example.com"},
+		Subject:     "test email with attachments",
+		Attachments: []string{"testdata/nullfile"},
+	})
+	require.Error(t, err)
+	require.Equal(t, "failed to write attachments: failed to read file type \"testdata/nullfile\": EOF",
+		err.Error())
+	require.Equal(t, "", msg)
+}
+
+func TestWriteAttachmentsFailed(t *testing.T) {
+
+	e := NewSender("localhost", ContentType("text/html"))
+	wc := &fakeWriterCloser{fail: true}
+	mp := multipart.NewWriter(wc)
+	err := e.writeAttachments(mp, []string{"testdata/1.txt"})
+	require.Error(t, err)
+}
+
+func TestWriteBody(t *testing.T) {
+	e := NewSender("localhost", ContentType("text/html"))
+	wc := &fakeWriterCloser{buff: &bytes.Buffer{}}
+	err := e.writeBody(wc, "this is a test 12345")
+	require.NoError(t, err)
+	assert.Equal(t, "this is a test 12345", wc.buff.String())
+}
+
+func TestWriteBodyFail(t *testing.T) {
+	e := NewSender("localhost", ContentType("text/html"))
+	wc := &fakeWriterCloser{fail: true}
+	err := e.writeBody(wc, "this is a test 12345")
+	require.Error(t, err)
+}
+
+// uncomment to debug with real smtp server
+// func TestSendIntegration(t *testing.T) {
+//	client := NewSender("localhost", ContentType("text/html"), Port(2525))
+//	err := client.Send("<html>some content, foo bar</html>",
+//		Params{From: "me@example.com", To: []string{"to@example.com"}, Subject: "Hello world!",
+//			Attachments: []string{"testdata/1.txt", "testdata/2.txt", "testdata/image.jpg"}})
+//	require.NoError(t, err)
+//}
 
 type fakeWriterCloser struct {
 	buff *bytes.Buffer
